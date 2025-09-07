4bb168c5bfa129294e7ae5b5e23becbe4f4ae0f0
mohamadalraie <mohamadalraie963@gmail.com>
2025-09-07 22:01:28 +0300

    b

--- src/scenes/engineers/Index.jsx
+++ src/scenes/engineers/Index.jsx
@@ -77,7 +77,7 @@ const Engineers = () => {
       flex: 0.5,
       renderCell: ({ row }) => (
         <Box display="flex" justifyContent="center" alignItems="center" height="100%" width="100%">
-          {havePermission("edit engineers") && (
+          {havePermission("edit enggineers") && (
             // This button now opens the dialog
             <IconButton
               aria-label="edit"
